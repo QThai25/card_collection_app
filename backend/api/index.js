e6685f774a65218baffc86179e124739f632439c
Qthai25 <thaivo307@gmail.com>
2026-01-26 13:51:23 +0700

    Test vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,15 @@ async function connectDB() {
   console.log("MongoDB connected");
 }
 
+
 export default async function handler(req, res) {
   await connectDB();
   return app(req, res);
 }
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    mongo: "connected",
+    time: new Date().toISOString(),
+  });
+});
