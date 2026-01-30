166d05d641e3b87403b43e951bbaa183cdc0b451
Qthai25 <thaivo307@gmail.com>
2026-01-30 12:53:53 +0700

    Last test for demo BE

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    dbName: "Card_DB",
+  })
   .then(() => {
-    console.log("✅ MongoDB connected");
+    console.log("✅ MongoDB connected to:", mongoose.connection.name);
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running at http://localhost:${PORT}`);
